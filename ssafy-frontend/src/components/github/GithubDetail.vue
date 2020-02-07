7184ea04c85687431f58aea7a54639bb907f6be3
wally-wally <wallys0213@gmail.com>
2020-02-07 17:53:49 +0900

    0207 | GH | customizing mypage_github_section2

--- ssafy-frontend/src/components/github/GithubDetail.vue
+++ ssafy-frontend/src/components/github/GithubDetail.vue
@@ -5,13 +5,13 @@
     </div>
     <div class="github-subinfo-1">
       <p><i class="far fa-building"></i>{{ githubInformation.company }}</p>
-      <p @click="goBlog(githubInformation.blog)"><i class="fas fa-blog"></i>{{ githubInformation.blog }}</p>
+      <p @click="goBlog(githubInformation.blog)"><i class="fas fa-blog"></i><span>{{ githubInformation.blog }}</span></p>
       <p><i class="fas fa-location-arrow"></i>{{ githubInformation.location }}</p>
     </div>
     <div class="github-subinfo-2 d-flex justify-space-between">
       <div class="repository-section text-center">
         <div>Repository Count</div>
-        <div>{{ githubInformation.publicReposCount }}</div>
+        <div @click.stop="reposDialog = true">{{ githubInformation.publicReposCount }}</div>
       </div>
       <div class="followers-section text-center">
         <div>Followers</div>
@@ -22,24 +22,64 @@
         <div>{{ githubInformation.following }}</div>
       </div>
     </div>
+
+    <!-- github public repository list 부분 -->
+    <v-dialog v-if="reposDialog" v-model="reposDialog" max-width="1200px">
+      <v-card class="pa-3">
+        <div class="repository-info">
+          <div class="repository-main-title">{{ githubInformation.githubID }}'s Public Repository</div>
+          <v-divider></v-divider>
+          <div class="repository-information" v-for="repos in this.reposInfo" :key="repos">
+            {{ repos.name }}
+          </div>
+          <!-- {{ this.reposInfo }} -->
+        </div>
+      </v-card>
+    </v-dialog>
+
   </div>
 </template>
 
 <script>
-
+import axios from 'axios'
 import '@/assets/css/GithubDetail.css'
+import '@/assets/css/GithubDetail_Repos.css'
+
 export default {
 	name: 'GithubDetail',
 	props: {
 		githubInformation: {type: Object}
 	},
 	data() {
-		return {}
+		return {
+      reposDialog: false,
+      reposInfo: null
+    }
   },
   methods: {
     goBlog: function(url) {
-      window.open(url)
+      if (url !== 'No Blog') {
+        window.open(url)
+      }
     }
+  },
+  mounted() {
+    axios.get(`https://api.github.com/users/${this.$store.getters.githubid}/repos`)
+      .then(response => {
+        const githubReposData = response.data
+        let reposArray = []
+        githubReposData.forEach(function(data) {
+          reposArray.push({
+            'name': data.name,
+            'reposUrl': data['html_url'],
+            'cloneUrl': data['clone_url'],
+            'description': data['description'],
+            'language': data['language']
+          })
+        })
+        this.reposInfo = reposArray
+      })
+      .catch(error => console.log(error))
   }
 }
 </script>
